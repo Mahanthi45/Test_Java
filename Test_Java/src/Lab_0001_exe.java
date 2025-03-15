4cd38dfd62a20322b88575d05638f6592c77aa57
HelloWorld <mahanthisivamanchireddy@gmail.com>
2025-03-15 15:33:33 +0530

    Push

---
+++ Test_Java/src/Lab_0001_exe.java
@@ -0,0 +1,5 @@
+public class Lab_0001_exe {
+    public static void main(String[] args) {
+        System.out.println("Lab 1");
+    }
+}
---
+++ Test_Java/src/test_0002_push.java
@@ -0,0 +1,6 @@
+public class test_0002_push {
+    public static void main(String[] args) {
+        System.out.println("Pushing code");
+    }
+}
+
